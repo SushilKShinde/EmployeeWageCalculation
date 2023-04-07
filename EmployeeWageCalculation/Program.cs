2b83921511b37546a3a09314a1b8fba01124bc11
Sushil <skshinde2028@gmail.com>
2023-04-07 17:18:58 +0530

    [Sushil] uploading file

---
+++ EmployeeWageCalculation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to check whether Employee is present or not program");
+            Console.ReadLine();
+        }
+    }
+}
