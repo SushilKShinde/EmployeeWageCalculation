060aff651d069e1b97748ee4cc927c5f610d9c97
Sushil <skshinde2028@gmail.com>
2023-04-07 18:08:58 +0530

    [Sushil] Modify Emp Attendance

--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -12,6 +12,22 @@ namespace EmployeeWageCalculation
         {
             Console.WriteLine("Welcome to check whether Employee is present or not program");
             Console.ReadLine();
+
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            int empAttendance = random.Next(0, 2);
+            Console.WriteLine("Value of random no: {0}",empAttendance);
+            Console.ReadLine();
+
+            if (empAttendance == 0)
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+            }
+            Console.ReadLine();
         }
     }
 }
