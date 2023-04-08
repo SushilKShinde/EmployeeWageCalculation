e21fde482b96a7e393a870fabdb7b439cb613e6d
Sushil <skshinde2028@gmail.com>
2023-04-08 14:04:53 +0530

    [Sushil] Emp daily wage calculation

--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -10,23 +10,28 @@ namespace EmployeeWageCalculation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to check whether Employee is present or not program");
+            Console.WriteLine("Welcome to Employee wage calculation  program");
             Console.ReadLine();
+            //UC2
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 50;
+            int empHrs = 0;
+            int empWage = 0;
 
-            int IS_PRESENT = 1;
             Random random = new Random();
             int empAttendance = random.Next(0, 2);
-            Console.WriteLine("Value of random no: {0}",empAttendance);
-            Console.ReadLine();
-
-            if (empAttendance == 0)
-            {
-                Console.WriteLine("Employee is absent");
-            }
-            else
+            if (empAttendance == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
+            else 
+            {
+                Console.WriteLine("Employee is absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee wage is: {0}", empWage);
             Console.ReadLine();
         }
     }
